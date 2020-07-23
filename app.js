9e8ba5d997789c5f3a3a4cde75d1a897485cc917
Bogarh451 <contacto.eduardo.rosas@gmail.com>
2020-07-23 18:26:38 -0500

    fdsf
    sfsdf

---
+++ app.js
@@ -0,0 +1,11 @@
+function muestraOpciones(){
+    var opciones = document.getElementById("radio1");
+
+        if(opciones.checked==true){
+            alert("Uno fue seleciconado");
+        } else {
+            alert("No fui seleccionaod");
+        }
+    }
+
+
